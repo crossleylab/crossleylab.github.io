cb478c1596728e74612a35f7ca5fd05c0bbef21c
mshah9 <2034771+mshah9@users.noreply.github.com>
2024-04-03 22:44:46 +1100

    test

--- _plugins/or_filter.rb
+++ _plugins/or_filter.rb
@@ -1,9 +1,11 @@
+require 'liquid'
+
 module Jekyll
-    class OrFilter < Liquid::Filter
-      def or(input, *args)
-        input || args.any? 
-      end
+  class OrFilter < Liquid::Filter
+    def or(input, *args)
+      input || args.any? 
     end
   end
+end
 
 Liquid::Template.register_filter('or', Jekyll::OrFilter)
