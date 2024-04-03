c4de83be139493bda79cf5598092fb8a9bd982de
mshah9 <2034771+mshah9@users.noreply.github.com>
2024-04-03 21:35:54 +1100

    test new filtering

--- _plugins/author_filter.rb
+++
@@ -1,9 +0,0 @@
-module Jekyll
-  module AuthorFilter
-    def has_author(input, author_name)
-      true  # Always return true for now
-    end
-  end
-end
-
-Liquid::Template.register_filter(Jekyll::AuthorFilter)
